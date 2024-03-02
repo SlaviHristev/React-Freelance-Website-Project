1572778d32ceb993af27aaf41a1b3b277392d9c7
SlaviHristev <Slavihristev97@gmail.com>
2024-03-02 19:31:58 +0200

    Created Navbar and styled it

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Navbar.scss"
+
+const Navbar =() =>{
+    return(
+        <div className="navbar">
+            <div className="container">
+                <div className="logo">
+                    <span className="text">fiverr</span>
+                    <span className="dot">.</span>
+                </div>
+                <div className="links">
+                    <span>Fiverr Business</span>
+                    <span>Explore</span>
+                    <span>English</span>
+                    <span>Sign In</span>
+                    <span>Become a Seller</span>
+                    <button>Join</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
