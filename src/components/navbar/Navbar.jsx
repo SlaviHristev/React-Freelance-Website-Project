97d12ab3dce9889e991d3902607aa6dfd11f8d7a
SlaviHristev <Slavihristev97@gmail.com>
2024-03-03 10:57:49 +0200

    Made the navbar sticky and transition when scrolled down

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.scss"
 import { Link } from "react-router-dom";
 
-const Navbar =() =>{
-    return(
-        <div className="navbar">
+const Navbar = () => {
+    const [active, setActive] = useState(false);
+
+    const isActive = () =>{
+        window.scrollY > 0 ? setActive(true) : setActive(false);
+    }
+
+    useEffect(() =>{
+        window.addEventListener('scroll', isActive);
+
+        return () =>{
+            window.removeEventListener('scroll', isActive)
+        }
+    },[])
+
+    return (
+        <div className={active ? "navbar active" : "navbar"}>
             <div className="container">
                 <div className="logo">
                     <Link to="/">
-                    <span className="text">fiverr</span>
+                        <span className="text">fiverr</span>
                     </Link>
                     <span className="dot">.</span>
                 </div>
@@ -21,11 +35,15 @@ const Navbar =() =>{
                     <button>Join</button>
                 </div>
             </div>
-                <hr />
-                <div className="menu">
-                    <span>Test1</span>
-                    <span>Test2</span>
-                </div>
+            {active  && (
+                <>
+                    <hr />
+                    <div className="menu">
+                        <span>Test1</span>
+                        <span>Test2</span>
+                    </div>
+                </>
+            )}
         </div>
     )
 }
