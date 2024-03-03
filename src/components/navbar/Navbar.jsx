2b2d48a453d4705c76b2e4d6f677ca6071d41e4c
SlaviHristev <Slavihristev97@gmail.com>
2024-03-03 11:16:03 +0200

    Finished working on the navbar adding a functionality for a current user

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     const [active, setActive] = useState(false);
+    const [open,setOpen] = useState(false);
 
     const isActive = () =>{
         window.scrollY > 0 ? setActive(true) : setActive(false);
@@ -17,13 +18,19 @@ const Navbar = () => {
         }
     },[])
 
+    const currentUser = {
+        id:1,
+        username:"Slavi Hr",
+        isSeller:true,
+    }
+
     return (
         <div className={active ? "navbar active" : "navbar"}>
             <div className="container">
                 <div className="logo">
-                    <Link to="/">
+                    {/* <Link to="/"> */}
                         <span className="text">fiverr</span>
-                    </Link>
+                    {/* </Link> */}
                     <span className="dot">.</span>
                 </div>
                 <div className="links">
@@ -31,8 +38,25 @@ const Navbar = () => {
                     <span>Explore</span>
                     <span>English</span>
                     <span>Sign In</span>
-                    <span>Become a Seller</span>
-                    <button>Join</button>
+                    {!currentUser?.isSeller && <span>Become a Seller</span>}
+                    {!currentUser && <button>Join</button>}
+                    {currentUser && (
+                        <div className="user" onClick={() => setOpen(!open)}>
+                            <img src="https://images.pexels.com/photos/5378700/pexels-photo-5378700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                            <span>{currentUser?.username}</span>
+                            {open && (<div className="options">
+                                {currentUser?.isSeller && (
+                                    <>
+                                    <span>Gigs</span>
+                                    <span>Add New Gig</span>
+                                    </>
+                                )}
+                                <span>Orders</span>
+                                <span>Messages</span>
+                                <span>Logout</span>
+                            </div>)}
+                        </div>
+                    )}
                 </div>
             </div>
             {active  && (
