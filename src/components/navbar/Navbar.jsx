749bc19454f211fdf0746b679216e83414c91e37
SlaviHristev <Slavihristev97@gmail.com>
2024-03-03 12:42:46 +0200

    Made and styled trusted by component in the home page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -31,12 +31,12 @@ const Navbar = () => {
             <div className="container">
                 <div className="logo">
                     <Link to="/" className="link">
-                        <span className="text">fiverr</span>
+                        <span className="text">Freelance4U</span>
                     </Link>
                     <span className="dot">.</span>
                 </div>
                 <div className="links">
-                    <span>Fiverr Business</span>
+                    <span>Freelance4U Business</span>
                     <span>Explore</span>
                     <span>English</span>
                     <span>Sign In</span>
---
+++ src/components/trustedBy/TrustedBy.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './TrustedBy.scss'
+
+const TrustedBy = () => {
+    return (
+        <div className="trustedBy">
+            <div className="container">
+            <span>Trusted by:</span>
+                <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/meta.99a0dda.png" alt="" />
+                <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/google.9d71a37.png" alt="" />
+                <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/netflix.35dc5cd.png" alt="" />
+                <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/pandg.967b1ad.png" alt="" />
+                <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/paypal.910e738.png" alt="" />
+            </div>
+        </div>
+    )
+}
+export default TrustedBy;
