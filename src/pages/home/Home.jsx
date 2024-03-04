d6512eb655a28886a75bb29e9151b9576ecf9e7e
SlaviHristev <Slavihristev97@gmail.com>
2024-03-04 15:11:26 +0200

    Added another slider for projects in the home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,18 +3,19 @@ import "./Home.scss"
 import Featured from "../../components/featured/Featured";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
 import Slide from "../../components/slide/Slide";
-import { cards } from "../../data";
+import { cards, projects } from "../../data";
 import CatCard from "../../components/catCard/CatCard";
+import ProjectCard from "../../components/projectCard/ProjectCard";
 
 
-const Home =() =>{
-    return(
+const Home = () => {
+    return (
         <div className='home'>
-            <Featured/>
-            <TrustedBy/>
+            <Featured />
+            <TrustedBy />
             <Slide slidesToShow={5} arrowsScroll={5}>
-                {cards.map(card =>(
-                    <CatCard item={card} key={card.id}/>
+                {cards.map(card => (
+                    <CatCard item={card} key={card.id} />
                 ))}
             </Slide>
             <div className="features">
@@ -51,47 +52,52 @@ const Home =() =>{
                         </p>
                     </div>
                     <div className="item">
-                    <video src="./img/video.mp4" controls></video>
+                        <video src="./img/video.mp4" controls></video>
                     </div>
                 </div>
             </div>
             <div className="features dark">
-        <div className="container">
-          <div className="item">
-            <h1>
-              Freelance4U <i>business</i>
-            </h1>
-            <h1>
-              A business solution designed for <i>teams</i>
-            </h1>
-            <p>
-              Upgrade to a curated experience packed with tools and benefits,
-              dedicated to businesses
-            </p>
-            <div className="title">
-              <img src="./img/check.png" alt="" />
-              Connect to freelancers with proven business experience
-            </div>
+                <div className="container">
+                    <div className="item">
+                        <h1>
+                            Freelance4U <i>business</i>
+                        </h1>
+                        <h1>
+                            A business solution designed for <i>teams</i>
+                        </h1>
+                        <p>
+                            Upgrade to a curated experience packed with tools and benefits,
+                            dedicated to businesses
+                        </p>
+                        <div className="title">
+                            <img src="./img/check.png" alt="" />
+                            Connect to freelancers with proven business experience
+                        </div>
 
-            <div className="title">
-              <img src="./img/check.png" alt="" />
-              Get matched with the perfect talent by a customer success manager
-            </div>
+                        <div className="title">
+                            <img src="./img/check.png" alt="" />
+                            Get matched with the perfect talent by a customer success manager
+                        </div>
 
-            <div className="title">
-              <img src="./img/check.png" alt="" />
-              Manage teamwork and boost productivity with one powerful workspace
+                        <div className="title">
+                            <img src="./img/check.png" alt="" />
+                            Manage teamwork and boost productivity with one powerful workspace
+                        </div>
+                        <button>Explore</button>
+                    </div>
+                    <div className="item">
+                        <img
+                            src="./img/features.png"
+                            alt=""
+                        />
+                    </div>
+                </div>
             </div>
-            <button>Explore</button>
-          </div>
-          <div className="item">
-            <img
-              src="./img/features.png"
-              alt=""
-            />
-          </div>
-        </div>
-            </div>
+            <Slide slidesToShow={4} arrowsScroll={4}>
+                {projects.map(project =>(
+                    <ProjectCard item={project} key={project.id}/>
+                ))}
+            </Slide>
         </div>
     )
 }
