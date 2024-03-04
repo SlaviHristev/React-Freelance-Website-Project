7075e7eb44af9e1231954c7f6d79a8f7e3b83569
SlaviHristev <Slavihristev97@gmail.com>
2024-03-04 16:00:28 +0200

    Started working on the category page

---
+++ src/components/gigCard/GigCard.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from './GigCard.scss'
+import { Link } from "react-router-dom";
+
+const GigCard = ({ item }) => {
+    return (
+        <Link to='/gig/1'>
+            <div className="gigCard">
+                <img src={item.img} alt="" />
+                <div className="info">
+                    <div className="user">
+                        <img src={item.pp} alt="" />
+                        <span>{item.username}</span>
+                    </div>
+                    <p>{item.desc}</p>
+                    <div className="star">
+                        <img src="./img/star.png" alt="" />
+                        <span>{item.star}</span>
+                    </div>
+                </div>
+                <div className="details">
+                    <img src='./img/heart.png' alt="" />
+                    <span>STARTING AT</span>
+                    <h2>${item.price}</h2>
+                </div>
+            </div>
+        </Link>
+    )
+}
+export default GigCard
