7902c74018963b46b95b3882bb84d9ca4e2a9989
SlaviHristev <Slavihristev97@gmail.com>
2024-03-09 11:48:22 +0200

    Created and styled myOrders page

--- src/pages/myOrders/MyOrders.jsx
+++ src/pages/myOrders/MyOrders.jsx
@@ -1,9 +1,66 @@
 import React from "react";
 import "./MyOrders.scss"
 
+
 const MyOrders =() =>{
+
+    const currentUser = {
+        id:1,
+        username: 'Slavi H',
+        isSeller:true,
+    }
+
     return(
-        <div className='myOrders'>MyOrders</div>
+        <div className='orders'>
+        <div className="container">
+        <div className="title">
+            <h1>
+                Orders
+            </h1>
+        </div>
+        <table>
+            <tr>
+                <th>Image</th>
+                <th>Title</th>
+                <th>Price</th>
+                <th>{currentUser?.isSeller ? "Buyer" : "Seller"}</th>
+                <th>Contact</th>
+            </tr>
+            <tr>
+                <td>
+                    <img className="img" src="https://images.pexels.com/photos/1029141/pexels-photo-1029141.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                </td>
+                <td>Gig1</td>
+                <td>29</td>
+                <td>123</td>
+                <td>
+                    <img className="delete" src="/img/message.png" alt="" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <img className="img" src="https://images.pexels.com/photos/1029141/pexels-photo-1029141.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                </td>
+                <td>Gig1</td>
+                <td>29</td>
+                <td>123</td>
+                <td>
+                    <img className="delete" src="/img/message.png" alt="" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <img className="img" src="https://images.pexels.com/photos/1029141/pexels-photo-1029141.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                </td>
+                <td>Gig1</td>
+                <td>29</td>
+                <td>123</td>
+                <td>
+                    <img className="delete" src="/img/message.png" alt="" />
+                </td>
+            </tr>
+        </table>
+        </div></div>
     )
 }
 
