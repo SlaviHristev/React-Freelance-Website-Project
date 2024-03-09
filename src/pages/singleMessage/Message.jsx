c71f6e82c32c26d92aac6c296eb264394dae50e6
SlaviHristev <Slavihristev97@gmail.com>
2024-03-09 12:49:31 +0200

    Created and styled the single message page

--- src/pages/singleMessage/Message.jsx
+++ src/pages/singleMessage/Message.jsx
@@ -1,9 +1,62 @@
 import React from "react";
 import "./Message.scss"
+import { Link } from "react-router-dom";
 
 const Message =() =>{
     return(
-        <div className='singleMessage'>SingleMessage</div>
+        <div className='message'>
+            <div className="container">
+                <span className="breadcrumbs">
+                    <Link to='/messages' >MESSAGES</Link> > SLAVI H > 
+                </span>
+                <div className="messages">
+                    <div className="item">
+                        <img src="https://images.pexels.com/photos/5378700/pexels-photo-5378700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur officiis illo expedita sequi provident voluptatem officia esse nam ad odit.
+                    </p>
+                    </div>
+
+                    <div className="item owner">
+                        <img src="https://images.pexels.com/photos/5378700/pexels-photo-5378700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur officiis illo expedita sequi provident voluptatem officia esse nam ad odit.
+                    </p>
+                    </div>
+                    <div className="item">
+                        <img src="https://images.pexels.com/photos/5378700/pexels-photo-5378700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur officiis illo expedita sequi provident voluptatem officia esse nam ad odit.
+                    </p>
+                    </div>
+
+                    <div className="item owner">
+                        <img src="https://images.pexels.com/photos/5378700/pexels-photo-5378700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur officiis illo expedita sequi provident voluptatem officia esse nam ad odit.
+                    </p>
+                    </div>
+                    <div className="item">
+                        <img src="https://images.pexels.com/photos/5378700/pexels-photo-5378700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur officiis illo expedita sequi provident voluptatem officia esse nam ad odit.
+                    </p>
+                    </div>
+
+                    <div className="item owner">
+                        <img src="https://images.pexels.com/photos/5378700/pexels-photo-5378700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur officiis illo expedita sequi provident voluptatem officia esse nam ad odit.
+                    </p>
+                    </div>
+                </div>
+                <hr />
+                <div className="write">
+                    <textarea name="" id="" placeholder="write a message" cols="30" rows="10"></textarea>
+                     <button>Send</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
