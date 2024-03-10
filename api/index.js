236acec67b82b29f0789801e134cc943b20e631c
SlaviHristev <Slavihristev97@gmail.com>
2024-03-10 11:29:14 +0200

    Setted up my routes and controllers

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 import express from 'express'
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import userRoute from './routes/user.js'
+import reviewRoute from './routes/review.js'
+import conversationRoute from './routes/conversation.js'
+import messageRoute from './routes/message.js'
+import gigRoute from './routes/gig.js'
+import orderRoute from './routes/order.js'
 
 const app = express();
 dotenv.config();
@@ -13,7 +19,15 @@ const connectDB = async() =>{
     } catch (error) {
         console.log(error);
     }
-}
+};
+
+app.use("/api/users", userRoute);
+app.use("/api/gigs", gigRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/reviews", reviewRoute);
+
 
 
 app.listen(6000, () =>{
