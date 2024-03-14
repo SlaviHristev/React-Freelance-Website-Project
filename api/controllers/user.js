e124c13a1ddef9de4e180bd7cad575a3e788d343
SlaviHristev <Slavihristev97@gmail.com>
2024-03-14 18:49:26 +0200

    Finished my jwt logic

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,19 +1,16 @@
 import User from "../models/User.js"
-import jwt from 'jsonwebtoken';
+
 
 export const deleteUser = async (req,res) =>{
     const user = await User.findById(req.params.id);
-    const token = req.cookies.accessToken;
-    if(!token){
-        return res.status(401).send("You are not authenticated!")
-    }
 
-    jwt.verify(token, process.env.JWT_KEY, async (err,payload) =>{
-        if(payload.id !== user._id){
+   
+
+        if(req.userId !== user._id.toString()){
             return res.status(403).send("You can delete only your account!")
         }
         await User.findByIdAndDelete(req.params.id);
-    })
+        res.status(200).send('Deleted!');
 
 
 }
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import {deleteUser} from '../controllers/user.js'
+import { verifyToken } from '../middlewares/jwt.js';
 
 const router = express.Router();
 
-router.delete("/:id",deleteUser)
+router.delete("/:id",verifyToken,deleteUser)
 
 
 export default router;
