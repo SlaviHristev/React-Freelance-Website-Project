001c76275ee80ea0561a49ecf94872167e1e7d18
SlaviHristev <Slavihristev97@gmail.com>
2024-03-16 13:00:02 +0200

    Started making gig functionality logic

--- api/routes/gig.js
+++ api/routes/gig.js
@@ -1,7 +1,13 @@
 
 import express from 'express';
+import { createGig, deleteGig, getGig, getGigs} from '../controllers/gig.js'
+import { verifyToken } from '../middlewares/jwt.js';
 
 const router = express.Router();
 
+router.post('/', verifyToken, createGig);
+router.delete('/:id', verifyToken, deleteGig);
+router.get('/single/:id', verifyToken, getGig);
+router.get('/', verifyToken, getGigs);
 
 export default router;
