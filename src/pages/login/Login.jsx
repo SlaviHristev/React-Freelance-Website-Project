d839e34a6e2d96586c459c6935d59f2362012e38
SlaviHristev <Slavihristev97@gmail.com>
2024-03-16 11:01:11 +0200

    Created and styled login and register pages

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,9 +2,26 @@ import React from "react";
 import "./Login.scss"
 
 const Login =() =>{
-    return(
-        <div className='login'>Login</div>
-    )
+      return (
+    <div className="login">
+      <form>
+        <h1>Sign in</h1>
+        <label htmlFor="">Username</label>
+        <input
+          name="username"
+          type="text"
+          placeholder="johndoe"
+        />
+
+        <label htmlFor="">Password</label>
+        <input
+          name="password"
+          type="password"
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,9 +2,63 @@ import React from "react";
 import "./Register.scss"
 
 const Register =() =>{
-    return(
-        <div className='register'>Register</div>
-    )
+    return (
+        <div className="register">
+          <form>
+            <div className="left">
+              <h1>Create a new account</h1>
+              <label htmlFor="">Username</label>
+              <input
+                name="username"
+                type="text"
+                placeholder="johndoe"
+              />
+              <label htmlFor="">Email</label>
+              <input
+                name="email"
+                type="email"
+                placeholder="email"
+              />
+              <label htmlFor="">Password</label>
+              <input name="password" type="password" />
+              <label htmlFor="">Profile Picture</label>
+              <input type="file" />
+              <label htmlFor="">Country</label>
+              <input
+                name="country"
+                type="text"
+                placeholder="Usa"
+              />
+              <button type="submit">Register</button>
+            </div>
+            <div className="right">
+              <h1>I want to become a seller</h1>
+              <div className="toggle">
+                <label htmlFor="">Activate the seller account</label>
+                <label className="switch">
+                  <input type="checkbox" />
+                  <span className="slider round"></span>
+                </label>
+              </div>
+              <label htmlFor="">Phone Number</label>
+              <input
+                name="phone"
+                type="text"
+                placeholder="+1 234 567 89"
+
+              />
+              <label htmlFor="">Description</label>
+              <textarea
+                placeholder="A short description of yourself"
+                name="desc"
+                id=""
+                cols="30"
+                rows="10"
+              ></textarea>
+            </div>
+          </form>
+        </div>
+      );
 }
 
 export default Register
