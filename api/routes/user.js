7bd05585b84c0d38357289fc3aa1bd763fc06cec
SlaviHristev <Slavihristev97@gmail.com>
2024-03-17 12:39:24 +0200

    Made changes on gigCard that should dispay proper data

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {deleteUser} from '../controllers/user.js'
+import {deleteUser, getUser} from '../controllers/user.js'
 import { verifyToken } from '../middlewares/jwt.js';
 
 const router = express.Router();
 
 router.delete("/:id",verifyToken,deleteUser)
+router.get("/:id",verifyToken,getUser)
 
 
 export default router;
--- src/components/gigCard/GigCard.jsx
+++ src/components/gigCard/GigCard.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 import './GigCard.scss'
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import newRequest from "../../utils/requests";
 
 const GigCard = ({ item }) => {
+
+    const { isLoading, error, data } = useQuery({
+        queryKey: ["gigUser"],
+        queryFn: () =>
+          newRequest
+            .get(
+              `/users/${item.userId}`
+            )
+            .then((res) => {
+              return res.data;
+            }),
+      });
+
     return (
         <Link to='/gig/1' className="link">
             <div className="gigCard">
-                <img src={item.img} alt="" />
+                <img src={item.cover} alt="" />
                 <div className="info">
+                   {isLoading ? "loading..." 
+                   : error ? "Something went wrong!" :
                     <div className="user">
-                        <img src={item.pp} alt="" />
-                        <span>{item.username}</span>
-                    </div>
+                        <img src={data.img || './img/man.png'} alt="" />
+                        <span>{data.username}</span>
+                    </div>}
                     <p>{item.desc}</p>
                     <div className="star">
                         <img src="./img/star.png" alt="" />
-                        <span>{item.star}</span>
+                        <span>{!isNaN(item.totalStars / item.starNumber) && Math.round(item.totalStars / item.starNumber)}</span>
                     </div>
                 </div>
                 <hr />
