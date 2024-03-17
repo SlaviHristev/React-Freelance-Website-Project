374d098a969fb31f43997e7039006c32465a0c31
SlaviHristev <Slavihristev97@gmail.com>
2024-03-17 11:14:11 +0200

    Added deleting and getting single or multiple gigs

--- api/controllers/gig.js
+++ api/controllers/gig.js
@@ -15,6 +15,35 @@ export const createGig = async(req,res,next) =>{
         next(error)
     }
 }
-export const deleteGig = async(req,res,next) =>{}
-export const getGig = async(req,res,next) =>{}
-export const getGigs = async(req,res,next) =>{}
+export const deleteGig = async(req,res,next) =>{
+    try {
+        const gig = await Gig.findById(req.params.id);
+
+        if(gig.userId !== req.userId) {
+            return next(createError(403, 'You can delete only your gig!'))
+        }
+        await Gig.findByIdAndDelete(req.params.id);
+        res.status(200).send("Gig has been deleted!")
+    } catch (error) {
+        next(error)
+    }
+}
+export const getGig = async(req,res,next) =>{
+    try {
+        const gig = await Gig.findById(req.params.id);
+        if(!gig){
+            next(createError(404,"Gig not found!"))
+        }
+        res.status(200).send(gig)
+    } catch (error) {
+        next(error)
+    }
+}
+export const getGigs = async(req,res,next) =>{
+    try {
+        const gigs = await Gig.find();
+        res.status(200).send(gigs)
+    } catch (error) {
+        next(error)
+    }
+}
