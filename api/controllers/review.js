40aa0ff4650aff14f696ad85abf205e863ffb8c4
SlaviHristev <Slavihristev97@gmail.com>
2024-03-21 19:44:09 +0200

    Started working on the api endpoint for reviews

---
+++ api/controllers/review.js
@@ -0,0 +1,25 @@
+import createError from "../utils/createError"
+
+export const createReview = async(req,res,next) =>{
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+export const getReviews = async(req,res,next) =>{
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+export const deleteReview = async(req,res,next) =>{
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
--- api/routes/review.js
+++ api/routes/review.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import { verifyToken } from '../middlewares/jwt';
+import { createReview, getReviews, deleteReview} from '../controllers/review.js'
 
 const router = express.Router();
+
+router.post('/', verifyToken, createReview);
+router.get('/:id', getReviews);
+router.delete('/:id', deleteReview);
 
 
 export default router;
